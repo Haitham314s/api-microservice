c9f6300aacd1509a9d489bdc274df4e61ad6bb5a
Haitham2169s <haitham@stretch.com>
2023-01-23 05:28:02 +0400

    Added order model + functions

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+@
+requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
+<
+Pipfile.lock,e\5\e563262209b7c2e62d1869582c8134583baa2204
+7
+Pipfile,1\5\15af4b3d30054104e85f65a444b1db188c5deefb
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- payment/main.py
+++ payment/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+
 from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+
+import requests, time
 
 app = FastAPI()
 app.add_middleware(
@@ -18,3 +23,52 @@ redis = get_redis_connection(
     decode_responses=True
 )
 
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # pending, completed, refunded
+
+    class Meta:
+        database = redis
+
+@app.get("/orders")
+async def get_all():
+    return [await get(pk) for pk in Order.all_pks()]
+
+
+
+@app.get("/orders/{pk}")
+async def get(pk: str):
+    return Order.get(pk)
+
+
+@app.post("/orders")
+async def create(request: Request, background_task: BackgroundTasks):
+    body = await request.json()
+
+    req = requests.get(f"http://localhost:8080/products/{body['id']}")
+    product = req.json()
+
+    order = Order(
+        product_id=body["id"],
+        price=product["price"],
+        fee=0.2 * product["price"],
+        total=1.2 * product["price"],
+        quantity=body["quantity"],
+        status="pending"
+    )
+    order.save()
+
+    background_task.add_task(order_completed, order)
+
+    return order
+
+
+async def order_completed(order: Order):
+    time.sleep(5)
+    order.status = "completed"
+    order.save()
